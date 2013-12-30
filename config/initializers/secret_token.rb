67b5c2ddef4a5049695adc249e1611003a30806a
Tim Skew <timskew@gmail.com>
2013-12-30 10:06:21 +0000

    Links pages together

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-TestApp::Application.config.secret_key_base = '3c45a54b9c0ffa8d5e666dfbee71beaaf6bc62ecd353710df9d032df923a94bb0a5fe1726860379b49520ae1fbff9a6fc8ad292591bddfdcc4da2b81ec03fc6a'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+TestApp::Application.config.secret_key_base = secure_token
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 TestApp::Application.routes.draw do
+  get "welcome/index"
   resources :data
 
   resources :suppliers
@@ -7,7 +8,7 @@ TestApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
